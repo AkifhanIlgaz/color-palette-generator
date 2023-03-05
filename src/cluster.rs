835df710ac9649599d2f0b63ae4da23b5e10757d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-03-05 21:49:32 +0300

    delete tests and some unnecessary lines

--- src/cluster.rs
+++ src/cluster.rs
@@ -9,7 +9,7 @@ impl Cluster {
     pub fn new(centroid: Color) -> Cluster {
         Cluster {
             centroid,
-            colors: vec![],
+            colors: vec![centroid],
         }
     }
 
@@ -19,10 +19,33 @@ impl Cluster {
     }
 
     pub fn set_new_centroid(&mut self) {
-        self.centroid = self.centroid.average_of_colors(&self.colors);
+        self.centroid = self.average_of_colors();
     }
 
     pub fn calculate_color_difference_with_centroid(&self, other_color: &Color) -> u32 {
         self.centroid.color_difference(other_color) as u32
+    }
+
+    pub fn average_of_colors(&self) -> Color {
+        let average_r = (self
+            .colors
+            .iter()
+            .map(|color| color.rgb[0] as u32)
+            .sum::<u32>()
+            / self.colors.len() as u32) as u8;
+        let average_g = (self
+            .colors
+            .iter()
+            .map(|color| color.rgb[1] as u32)
+            .sum::<u32>()
+            / self.colors.len() as u32) as u8;
+        let average_b = (self
+            .colors
+            .iter()
+            .map(|color| color.rgb[2] as u32)
+            .sum::<u32>()
+            / self.colors.len() as u32) as u8;
+
+        Color::new(average_r, average_g, average_b)
     }
 }
--- src/colors.rs
+++ src/colors.rs
@@ -5,15 +5,11 @@ use termion::{color, style};
 #[derive(Clone, Copy, PartialEq)]
 pub struct Color {
     pub rgb: [u8; 3],
-    pub lab: lab::Lab,
 }
 
 impl Color {
     pub fn new(r: u8, g: u8, b: u8) -> Color {
-        Color {
-            rgb: [r, g, b],
-            lab: lab::Lab::from_rgb(&[r, g, b]),
-        }
+        Color { rgb: [r, g, b] }
     }
 
     pub fn to_hex_string(&self) -> String {
@@ -25,18 +21,9 @@ impl Color {
     }
 
     pub fn color_difference(&self, other_color: &Color) -> f32 {
-        self.lab.squared_distance(&other_color.lab).sqrt()
-    }
-
-    pub fn average_of_colors(&self, colors: &Vec<Color>) -> Color {
-        let average_r = (colors.iter().map(|color| color.rgb[0] as u32).sum::<u32>()
-            / colors.len() as u32) as u8;
-        let average_g = (colors.iter().map(|color| color.rgb[1] as u32).sum::<u32>()
-            / colors.len() as u32) as u8;
-        let average_b = (colors.iter().map(|color| color.rgb[2] as u32).sum::<u32>()
-            / colors.len() as u32) as u8;
-
-        Self::new(average_r, average_g, average_b)
+        let self_lab = lab::Lab::from_rgb(&self.rgb);
+        let other_color_lab = lab::Lab::from_rgb(&other_color.rgb);
+        self_lab.squared_distance(&other_color_lab).sqrt()
     }
 }
 
@@ -44,10 +31,7 @@ impl From<Rgba<u8>> for Color {
     fn from(rgba: Rgba<u8>) -> Self {
         let mut rgb: [u8; 3] = [0; 3];
         rgb.copy_from_slice(&rgba.0[..3]);
-        Self {
-            rgb,
-            lab: lab::Lab::from_rgba(&rgba.0),
-        }
+        Self { rgb }
     }
 }
 
@@ -62,50 +46,3 @@ impl Display for Color {
     }
 }
 
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_creating_color_from_rgb_values() {
-        let color = Color::new(27, 41, 53);
-        assert_eq!([27, 41, 53], color.rgb);
-        assert_eq!(15.88, (color.lab.l * 1000.).round() / 1000.);
-        assert_eq!(-2.018, (color.lab.a * 1000.).round() / 1000.);
-        assert_eq!(-9.631, (color.lab.b * 1000.).round() / 1000.);
-    }
-
-    #[test]
-    fn test_creating_color_from_rgba() {
-        let rgba = Rgba::from([27_u8, 41, 53, 1]);
-        let color = Color::from(rgba);
-        assert_eq!([27, 41, 53], color.rgb);
-        assert_eq!(15.88, (color.lab.l * 1000.).round() / 1000.);
-        assert_eq!(-2.018, (color.lab.a * 1000.).round() / 1000.);
-        assert_eq!(-9.631, (color.lab.b * 1000.).round() / 1000.);
-    }
-
-    #[test]
-    fn test_hex_string() {
-        let color = Color::new(27, 41, 53);
-        assert_eq!("#1B2935".to_string(), color.to_hex_string());
-    }
-
-    #[test]
-    fn test_rgb_string() {
-        let color = Color::new(27, 41, 53);
-        assert_eq!("rgb(27,41,53)".to_string(), color.to_rgb_string());
-    }
-
-    #[test]
-    fn test_color_difference() {
-        let color = Color::new(21, 45, 78);
-        let color2 = Color::new(42, 186, 210);
-
-        assert_eq!(
-            60.221,
-            (color.color_difference(&color2) * 1000.).round() / 1000.
-        )
-    }
-}
